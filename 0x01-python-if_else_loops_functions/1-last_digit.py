fa4608bdef92078e4f93d2d0180849911c382d59
VicOwiti <hunkyvee31@gmail.com>
2021-09-07 13:48:08 +0000

    checking if a generated random number is reater than 5 or zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,17 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_number = number % 10
+to_string = str(number)
+last_number_string = to_string[-1]
+last_number = int(last_number_string)
 
 if last_number == 0:
     print("Last digit of {0} is {1} and is 0".format(number, last_number))
 
-if last_number > 5:
+elif last_number > 5:
     print("Last digit of {0} is {1} and is \
-    greater than 5".format(number, last_number))
+greater than 5".format(number, last_number))
 
-if last_number < 6 and not 0:
-    print("Last digit of {0} is {1} and is 0".format(number, last_number))
+elif last_number < 6 and not 0:
+    print("Last digit of {0} is {1} and is \
+less than 6 and not 0".format(number, last_number))
