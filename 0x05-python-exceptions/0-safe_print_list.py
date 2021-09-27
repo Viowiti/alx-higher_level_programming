b51f1ee045f9eab54f1b2e7d2a19ad1e987c95e4
VictorOwiti <hunkyvee31@gmail.com>
2021-09-27 06:25:52 -0700

    logic done, spaces pending

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,5 +4,14 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         newlist = my_list[:x]
+    except NameError:
+        sys.exit(1)
+    except TypeError:
+        sys.exit(1)
+    else:
+        counter = 0
         for i in newlist:
+            counter += 1
             print(i, end="")
+
+    return counter
