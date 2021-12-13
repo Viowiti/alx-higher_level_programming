2c168c6eeba37b343d309b77d03d0ca3d6fc37f3
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 13:56:13 +0300

    semistandard applied

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,6 +5,6 @@ const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 //  Loop through to print the contents of the array.
 
-for (let i = 0; i < myArr.length; i++){
-    console.log(myArr[i]);
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
 }
