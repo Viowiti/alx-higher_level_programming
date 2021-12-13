4a1a69ac9a23daa4b7fa999cb370243e7ac63284
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 13:10:21 +0300

    standard spacing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Print three statements to the console
+//  Print three statements to the console
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
