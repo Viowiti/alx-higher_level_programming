2b8ff68fa70e1bf9de5d8fe6e64d6d5beaab92b6
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 20:33:20 +0300

    altering an object value

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+//  Reassign the value of the object property
+myObject.value = 89;
+console.log(myObject);
