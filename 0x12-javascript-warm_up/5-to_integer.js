9280c38ebed15a9f7e987b475238ca8b7a28db45
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 18:13:23 +0300

    convert to unteger

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArr = process.argv;
+const firstArg = myArr[2];
+const converted = parseInt(firstArg);
+
+if (!Number.isNaN(converted)) {
+  console.log(`My number : ${converted}`);
+} else {
+    console.log('Not a number');
+}
