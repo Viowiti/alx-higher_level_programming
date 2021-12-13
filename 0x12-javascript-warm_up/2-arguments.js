7b2010c244e6f63c1ae46fe5fbc5b75a423f8ad6
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 14:52:42 +0300

    node command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
+//  Storing the arguments array in a variable.
+const argsArray = process.argv;
 
+if (argsArray.length < 3) {
+  console.log('No argument');
+}else if (argsArray.length === 3) {
+  console.log('Argument found');
+}else {
+  console.log('Arguments found');
+}
