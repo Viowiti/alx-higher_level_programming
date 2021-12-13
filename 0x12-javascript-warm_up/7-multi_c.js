58171ee4cf13b218ebf601cd1fc62cbb8233e82e
Victor Owiti <hunkyvee31@gmail.com>
2021-12-13 18:47:41 +0300

    making files excecutable

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const myArr = process.argv;
+const firstArg = myArr[2];
+const converted = parseInt(firstArg);
+
+if (!Number.isNaN(converted)) {
+    console.log(`Missing number of occurrences`);
+} else if (converted < 1) {
+  
+} else {
+    let x = 0
+    while (x < converted) {
+      console.log('C is fun');
+      x++;
+
+}
+}
