64875de498b03d96347e517bc2987e2c61435f3a
Victor Owiti <hunkyvee31@gmail.com>
2021-12-14 20:28:57 +0300

    mapping over an imported array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const myList = require('100-data.js').list;
-
-const byIndex = () => {
-  return myList.map((item) => item * myList.indexOf(item));
-};
+const newList = myList.map((item) => item * myList.indexOf(item));
 console.log(myList);
-console.log(byIndex());
+console.log(newList);
