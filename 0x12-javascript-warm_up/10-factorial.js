5837a2c40a64c673381409605efad1f259719b95
Victor Owiti <hunkyvee31@gmail.com>
2021-12-15 13:08:45 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@ const argArr = process.argv;
 const argOne = argArr[2];
 const toInt = parseInt(argOne);
 
-const factorial = (n) => {
+function factorial (n) {
   if (n === 0 || n === 1 || Number.isNaN(n) === true) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
-};
+}
 console.log(factorial(toInt));
