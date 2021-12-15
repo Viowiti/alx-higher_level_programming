bc59eae56e3db7ab556ec560f0a9be2e8d5d87e3
Victor Owiti <hunkyvee31@gmail.com>
2021-12-15 11:51:01 +0300

    removed extra space

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const firstArg = myArr[2];
 const converted = parseInt(firstArg);
 
 if (!Number.isNaN(converted)) {
-  console.log(`My number : ${converted}`);
+  console.log(`My number: ${converted}`);
 } else {
   console.log('Not a number');
 }
