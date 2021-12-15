4e959cac18a8a867ae54ab788053441882d24323
Victor Owiti <hunkyvee31@gmail.com>
2021-12-15 12:03:34 +0300

    changed boolean value

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,15 +4,14 @@ const myArr = process.argv;
 const firstArg = myArr[2];
 const converted = parseInt(firstArg);
 
-if (!Number.isNaN(converted)) {
-    console.log(`Missing number of occurrences`);
+if (Number.isNaN(converted)) {
+  console.log('Missing number of occurrences');
 } else if (converted < 1) {
-  
-} else {
-    let x = 0
-    while (x < converted) {
-      console.log('C is fun');
-      x++;
 
-}
+} else {
+  let x = 0;
+  while (x < converted) {
+    console.log('C is fun');
+    x++;
+  }
 }
