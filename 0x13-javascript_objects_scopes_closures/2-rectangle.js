8c2741c5dc7288277ee008f06ce4526a825a2c54
Victor Owiti <hunkyvee31@gmail.com>
2021-12-15 16:23:47 +0300

    conditionally create a class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
+  constructor (w, h) {
+    if (parseInt(w) > 0 || parseInt(h) > 0) {
       this.width = w;
       this.height = h;
     }
-  };
+    this.width = w;
+    this.height = h;
+  }
+};
